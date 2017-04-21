1da8da50c20b7d455d2c7d23124558903f122e84
AndreSouto <andre_luisferreira@hotmail.com>
2017-04-21 13:55:03 -0300

    Funcao recebeString() passou no teste.

--- src/romdec.cpp
+++ src/romdec.cpp
@@ -3,5 +3,29 @@
 
 char *recebeString(){ //Função que recebe string do usuário e valida o tamanho da mesma
 
-	
+	char *string_retornada = (char*)malloc(sizeof(char));
+
+	printf("Insira um valor em numeros romanos\n");
+	scanf("%s",string_retornada);
+
+	int contador = 0;
+	while(*(string_retornada + contador) != '\0'){
+			
+		contador++; 
+	}
+
+	while(contador >= 29){
+
+		printf("Tamanho maior que 30.\n");
+		printf("Insira um valor em numeros romanos\n");
+		scanf("%s",string_retornada);
+
+		int contador = 0;
+		while(*(string_retornada + contador) != '\0'){
+			
+			contador++;
+		}
+	}
+
+	return string_retornada;
 }
